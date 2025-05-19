6847d06364dc89807bd163d5c41ddec77cad753f
Livije09 <veljko.stojadinovic.mes@gmail.com>
2025-05-19 11:20:25 +0200

    produzeci

--- src/js/views/MatchView.js
+++ src/js/views/MatchView.js
@@ -9,9 +9,19 @@ export class MatchView extends View {
   #overlay = document.querySelector(".overlay");
   #firstHalfContainer = document.querySelector(".first-half");
   #secondHalfContainer = document.querySelector(".second-half");
+  #firstExtraHalfContainer = document.querySelector(".first-extra-half");
+  #secondExtraHalfContainer = document.querySelector(".second-extra-half");
   #score = {
     home: 0,
     away: 0,
+    firstExtraHalf: {
+      home: 0,
+      away: 0,
+    },
+    secondExtraHalf: {
+      home: 0,
+      away: 0,
+    },
   };
 
   constructor() {
@@ -125,11 +135,17 @@ export class MatchView extends View {
   #clearHalfs() {
     this.#firstHalfContainer.innerHTML = "";
     this.#secondHalfContainer.innerHTML = "";
+    this.#firstExtraHalfContainer.innerHTML = "";
+    this.#secondExtraHalfContainer.innerHTML = "";
   }
 
   #resetScore() {
     this.#score.home = 0;
     this.#score.away = 0;
+    this.#score.firstExtraHalf.home = 0;
+    this.#score.firstExtraHalf.away = 0;
+    this.#score.secondExtraHalf.home = 0;
+    this.#score.secondExtraHalf.away = 0;
   }
 
   #shortenTheName(name) {
@@ -147,22 +163,51 @@ export class MatchView extends View {
   }
 
   #showSVG(event) {
-    if (!event.detail.toLowerCase().includes("Substitution"))
+    if (!event.detail.toLowerCase().includes("substitution"))
       return SVGS[event.detail.toLowerCase().replace(/\s+/g, "")];
     return SVGS[event.detail.toLowerCase().split(" ")[0]];
+  }
+
+  #checkAssist(event) {
+    if (event.assist.id === null || event.assist.name === null) return "";
+    return `<p class="assist player">(${this.#shortenTheName(
+      event.assist.name
+    )})</p>`;
+  }
+
+  #addGoal(match, event) {
+    if (event.type === "Goal")
+      if (event.team.id === match.teams.home.id) {
+        this.#score.home++;
+        if (
+          Math.abs(event.time.elapsed) > 90 &&
+          Math.abs(event.time.elapsed) <= 105
+        )
+          this.#score.firstExtraHalf.home++;
+        if (Math.abs(event.time.elapsed) > 105)
+          this.#score.secondExtraHalf.home++;
+      } else {
+        this.#score.away++;
+        if (
+          Math.abs(event.time.elapsed) > 90 &&
+          Math.abs(event.time.elapsed) <= 105
+        )
+          this.#score.firstExtraHalf.away++;
+        if (Math.abs(event.time.elapsed) > 105)
+          this.#score.secondExtraHalf.away++;
+      }
   }
 
   #generateEventHTML(match) {
     const halfs = {
       firstHalf: "",
       secondHalf: "",
+      firstExtraHalf: "",
+      secondExtraHalf: "",
     };
     match.events.forEach((event) => {
-      if (event.type === "Goal")
-        event.team.id === match.teams.home.id
-          ? this.#score.home++
-          : this.#score.away++;
-      const html = `<div class="first-half-details details">
+      this.#addGoal(match, event);
+      const html = `<div class="details">
                 <div class="detail ${
                   event.team.id === match.teams.home.id
                     ? "home-detail"
@@ -181,18 +226,21 @@ export class MatchView extends View {
                   <p class="scorer player">${this.#shortenTheName(
                     event.player.name
                   )}</p>
-                  ${
-                    event.assist.id === null
-                      ? `<p class="assist player">(${this.#shortenTheName(
-                          event.assist.name
-                        )})</p>`
-                      : ""
-                  }
+                  ${this.#checkAssist(event)}
                 </div>
               </div>`;
-      Math.abs(event.time.elapsed) <= 45
-        ? (halfs.firstHalf += html)
-        : (halfs.secondHalf += html);
+      if (Math.abs(event.time.elapsed) <= 45) halfs.firstHalf += html;
+      if (
+        Math.abs(event.time.elapsed) > 45 &&
+        Math.abs(event.time.elapsed) <= 90
+      )
+        halfs.secondHalf += html;
+      if (
+        Math.abs(event.time.elapsed) > 90 &&
+        Math.abs(event.time.elapsed) <= 105
+      )
+        halfs.firstExtraHalf += html;
+      if (Math.abs(event.time.elapsed) > 105) halfs.secondExtraHalf += html;
     });
     return halfs;
   }
@@ -200,7 +248,8 @@ export class MatchView extends View {
   generateMatchDetails(match) {
     this.#clearHalfs();
     this.#resetScore();
-    const { firstHalf, secondHalf } = this.#generateEventHTML(match);
+    const { firstHalf, secondHalf, firstExtraHalf, secondExtraHalf } =
+      this.#generateEventHTML(match);
     const htmlFirstHalf = `<div class="first-half-header">
                     <p class="first-half-header-p">1. Half</p>
                     <p class="first-half-header-p">${match.score.halftime.home} - ${match.score.halftime.away}</p>
@@ -215,6 +264,37 @@ export class MatchView extends View {
                   ${secondHalf}
      `;
     this.#secondHalfContainer.insertAdjacentHTML("beforeend", htmlSecondHalf);
+    if (
+      !match.score.extratime.home === null &&
+      !match.score.extratime.away === null
+    ) {
+      const htmlFirstExtraHalf = `
+      <div class="first-extra-half-header">
+      <p class="first-extra-half-header-p">1. Extra half</p>
+      <p class="first-extra-half-header-p">${
+        this.#score.firstExtraHalf.home
+      } - ${this.#score.firstExtraHalf.away}</p>
+        </div>
+        ${firstExtraHalf}
+        `;
+      this.#firstExtraHalfContainer.insertAdjacentHTML(
+        "beforeend",
+        htmlFirstExtraHalf
+      );
+      const htmlSecondExtraHalf = `
+        <div class="second-extra-half-header">
+        <p class="second-extra-half-header-p">2. Extra half</p>
+        <p class="second-extra-half-header-p">${
+          this.#score.secondExtraHalf.home
+        } - ${this.#score.secondExtraHalf.away}</p>
+          </div>
+          ${secondExtraHalf}
+          `;
+      this.#secondExtraHalfContainer.insertAdjacentHTML(
+        "beforeend",
+        htmlSecondExtraHalf
+      );
+    }
   }
 }
 
